fc56e40a052d228c9dce9455487febe04d3fe2b1
genzouw <genzouw@gmail.com>
2018-04-15 12:11:38 +0900

    計算機能の実装完了。ボタンデザインも微調整.

--- app/src/main/java/com/genzouw/android_calcurator/MainActivity.java
+++ app/src/main/java/com/genzouw/android_calcurator/MainActivity.java
@@ -1,14 +1,19 @@
 package com.genzouw.android_calcurator;
 
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.util.ArrayList;
-import java.util.List;
+import net.java.dev.eval.Expression;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,30 +22,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Map<Integer, String> appendCharsMap = new HashMap<Integer, String>() {
+            {
+                this.put(R.id.button_0, "0");
+                this.put(R.id.button_1, "1");
+                this.put(R.id.button_2, "2");
+                this.put(R.id.button_3, "3");
+                this.put(R.id.button_4, "4");
+                this.put(R.id.button_5, "5");
+                this.put(R.id.button_6, "6");
+                this.put(R.id.button_7, "7");
+                this.put(R.id.button_8, "8");
+                this.put(R.id.button_9, "9");
+                this.put(R.id.button_dot, ".");
+                this.put(R.id.button_add, "+");
+                this.put(R.id.button_subtract, "-");
+                this.put(R.id.button_multiple, "*");
+                this.put(R.id.button_devide, "/");
+            }
+        };
+
         final EditText displayNumber = this.findViewById(R.id.number_display);
 
         View.OnClickListener clickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Button clickedButton = (Button) v;
-                displayNumber.append(clickedButton.getText());
+                switch (v.getId()) {
+                    case R.id.button_dot:
+                    case R.id.button_add:
+                    case R.id.button_subtract:
+                    case R.id.button_multiple:
+                    case R.id.button_devide:
+                        displayNumber.setText(displayNumber.getText().toString().replaceAll("[^0-9]$", ""));
+                        break;
+                }
+
+                displayNumber.append(appendCharsMap.get(v.getId()));
             }
         };
 
-        List<Integer> buttonIds = new ArrayList();
-        buttonIds.add(R.id.button_0);
-        buttonIds.add(R.id.button_1);
-        buttonIds.add(R.id.button_2);
-        buttonIds.add(R.id.button_3);
-        buttonIds.add(R.id.button_4);
-        buttonIds.add(R.id.button_5);
-        buttonIds.add(R.id.button_6);
-        buttonIds.add(R.id.button_7);
-        buttonIds.add(R.id.button_8);
-        buttonIds.add(R.id.button_9);
-        buttonIds.add(R.id.button_dot);
-
-        for (Integer buttonId : buttonIds) {
+        for(Integer buttonId : appendCharsMap.keySet()) {
             this.findViewById(buttonId).setOnClickListener(clickListener);
         }
 
@@ -52,30 +73,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button buttonAdd = this.findViewById(R.id.button_add);
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Button clickedButton = (Button) v;
-                displayNumber.append("+");
-            }
-        });
-
-        Button buttonSubtract = this.findViewById(R.id.button_subtract);
-        buttonSubtract.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Button clickedButton = (Button) v;
-                displayNumber.append("-");
-            }
-        });
-
         Button buttonAnswer = this.findViewById(R.id.button_answer);
         buttonAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Button clickedButton = (Button) v;
-                displayNumber.append(Eval);
+                try {
+                    BigDecimal answer =  Expression.eval(displayNumber.getText().toString());
+                    displayNumber.setText(answer.toString());
+                } catch (Exception e) {
+                    displayNumber.setText("");
+                }
             }
         });
 
